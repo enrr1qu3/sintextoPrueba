d3b6ab719e36d7e0621781ff0aa4df37067d801f
Enrique Alejo <enriquealejo28@gmail.com>
2024-04-27 11:38:53 -0600

    cambio de ruta

--- src/services/global.ts
+++ src/services/global.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://sintexto-api-dev.azurewebsites.net/api'; // Dev
-export const baseUrl = 'https://sintexto-api-production.azurewebsites.net/api'; // Production 
+export const baseUrl = 'https://sintexto-api-dev.azurewebsites.net/api'; // Dev
+// export const baseUrl = 'https://sintexto-api-production.azurewebsites.net/api'; // Production 
