71a4ed0d43e063bfb3c48e71be1d50c3057fe392
Enrique <enriquealejo28@gmail.com>
2024-04-27 19:10:36 -0600

    text 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import { MainArticle } from "@/components";
-import { getFrontPageLayoutPublished } from "@/services";
+// import { getFrontPageLayoutPublished } from "@/services";
 import { IFrontPageLayout } from "@/interfaces";
 
 
 // Obtiene el ForontPageLayout actualmente publicado
+// const getFrontPageLayout = async () => {
+//     const response = await getFrontPageLayoutPublished();    
+//     return response.data;
+// }
 const getFrontPageLayout = async () => {
-    const response = await getFrontPageLayoutPublished();    
-    return response.data;
+    try {
+        const res = await fetch(`https://sintexto-api-dev.azurewebsites.net/api/FrontPageLayout/UltimateFrontPageLayoutPublished`,{
+          cache: 'no-store',
+          next: {
+            revalidate: 2
+          }
+        }).then( resp => resp.json() );
+      
+        return res.data;
+        
+      } catch (error) {
+      
+      }
 }
 
 export default async function HomePage() {
