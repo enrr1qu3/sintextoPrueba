6f47ddf0c29b153832e926dd82c5cd6e590d0758
Miguel Angel López Luciano <ma.lopezluciano@gmail.com>
2024-04-29 11:41:37 -0600

    Correccion objeto header

--- src/services/frontPageLayoutService.ts
+++ src/services/frontPageLayoutService.ts
@@ -1,9 +1,15 @@
 import { axiosInstance } from "./axios";
-import { headers } from "./headers-cache";
 
 export const getFrontPageLayoutPublished = async () => {
     try {
-        const response = await axiosInstance.get('/FrontPageLayout/UltimateFrontPageLayoutPublished', headers);
+        const response = await axiosInstance.get('/FrontPageLayout/UltimateFrontPageLayoutPublished', {
+            // query URL without using browser cache
+            headers: {
+              'Cache-Control': 'no-cache',
+              'Pragma': 'no-cache',
+              'Expires': '0',
+            },
+        });
         if (response.status !== 200) {
             throw new Error('Error en la petición');
         }
--- src/services/publicNewsService.ts
+++ src/services/publicNewsService.ts
@@ -53,7 +53,14 @@ export const getNewsSection = async (idSections: any) => {
             return `Sections=${item.id}`
         }).join('&');
 
-        const response = await axiosInstance.get(`/PublicNews/GetSectionNews${queryParams}&OnlyFrontPageNews=true`, headers);
+        const response = await axiosInstance.get(`/PublicNews/GetSectionNews${queryParams}&OnlyFrontPageNews=true`, {
+            // query URL without using browser cache
+            headers: {
+              'Cache-Control': 'no-cache',
+              'Pragma': 'no-cache',
+              'Expires': '0',
+            },
+        });
         if (response.status !== 200) {
             throw new Error('Error en la petición');
         }
@@ -119,7 +126,14 @@ export const getSearch = async (params?: { NameSearch?: string, PageSize?:number
 
 export const getSocialMediaArticles = async () => {
     try {
-        const response = await axiosInstance.get('/PublicNews/GetSocialMediaArticles', headers);
+        const response = await axiosInstance.get('/PublicNews/GetSocialMediaArticles', {
+            // query URL without using browser cache
+            headers: {
+              'Cache-Control': 'no-cache',
+              'Pragma': 'no-cache',
+              'Expires': '0',
+            },
+        });
         if (response.status !== 200) {
             throw new Error('Error en la petición');
         }
@@ -157,7 +171,14 @@ export const getAdvertisementShort = async (relevance?: boolean, position?: stri
             queryParams += `&SectionId=${sectionId}`;
         }
         
-        const response = await axiosInstance.get(`/PublicNews/GetAdvertisementShort?OnlyValidAds=true${queryParams}`, headers);
+        const response = await axiosInstance.get(`/PublicNews/GetAdvertisementShort?OnlyValidAds=true${queryParams}`, {
+            // query URL without using browser cache
+            headers: {
+              'Cache-Control': 'no-cache',
+              'Pragma': 'no-cache',
+              'Expires': '0',
+            },
+        });
         if (response.status !== 200) {
             throw new Error('Error en la petición');
         }
