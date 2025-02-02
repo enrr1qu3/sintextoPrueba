b14143493d82764ce03f41aeff983888284add93
Enrique <enriquealejo28@gmail.com>
2025-02-02 09:58:30 -0600

    quitado el use Client de TextoArticle

--- src/components/article/TextoArticle.tsx
+++ src/components/article/TextoArticle.tsx
@@ -1,8 +1,4 @@
-'use client'
-// import DOMPurify from 'dompurify';
 import DOMPurify from "isomorphic-dompurify";
-
-
 export const TextoArticle = ({ texto }: { texto: string }) => {
   if (!texto) return null; // Agrega manejo de errores o validación de entrada
   return (
