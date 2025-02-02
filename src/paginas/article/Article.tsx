fa374e1d971f3cecb4210094a8c6a171df735961
Enrique <enriquealejo28@gmail.com>
2025-02-02 11:48:46 -0600

    se quitaron las clases de Article

--- src/paginas/article/Article.tsx
+++ src/paginas/article/Article.tsx
@@ -19,20 +19,20 @@ export const Article = (props: any) => {
     return (
         <>
             <TitleSectionHeader title={section.name} color={section.assignedColor} />
-            <Row >
+            <Row className="main-row">
 
                 <Col xs={0} lg={3} xl={4}>
-                    <div >
+                    <div className="col-ad-left">
                         {verticalAd?.length! > 0 && <AdvertisementCarousel advertisementV={verticalAd} numberInitial={0} />}
                         {verticalAd?.length! > 2 && <AdvertisementCarousel advertisementV={verticalAd} numberInitial={2} />}
                     </div>
                 </Col>
 
                 <Col xs={24} lg={18} xl={16}>
-                    <div >
+                    <div className="container">
                         <Row>
                             <Col xs={24} md={24}>
-                                <div >
+                                <div className="container-article">
                                     {/* carta y compartir */}
                                     <Row gutter={[0, 30]} justify="space-between">
                                         <CardAutorArticle authors={autorInfo} fecha={article.schedulePostDate} />
@@ -42,13 +42,13 @@ export const Article = (props: any) => {
 
                                     {/* informacion */}
 
-                                    <h2 >{article.title}</h2>
+                                    <h2 className="article-title">{article.title}</h2>
 
-                                    <h4 >{article.subtitle}</h4>
+                                    <h4 className="article-subtitle">{article.subtitle}</h4>
 
                                     <ContenVideoImg articleVI={article} />
 
-                                    <p  key={"imagenes"}>
+                                    <p className="subtitle-video" key={"imagenes"}>
                                         {article.subtitleVideo}
                                     </p>
 
@@ -82,7 +82,7 @@ export const Article = (props: any) => {
                 </Col>
 
                 <Col xs={0} lg={3} xl={4}>
-                    <div >
+                    <div className="col-ad-right">
                         {verticalAd?.length! > 1 && <AdvertisementCarousel advertisementV={verticalAd} numberInitial={1} />}
                         {verticalAd?.length! > 3 && <AdvertisementCarousel advertisementV={verticalAd} numberInitial={3} />}
                     </div>
@@ -91,7 +91,7 @@ export const Article = (props: any) => {
             </Row >
 
             {/* articulos relacionados */}
-            <div  >
+            <div className="container" >
                 <SectionHome
                     sectionTitle="Artículos relacionados"
                     articles={articlesFiltered}
